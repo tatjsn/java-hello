de2509b6dd3a63cb2d348cf8434de30856827ac8
Taz Jackson <tatjsn@users.noreply.github.com>
2020-10-05 22:40:35 +0100

    Add debug prints

--- heroku.sh
+++ heroku.sh
@@ -1,8 +1,12 @@
 mkdir app
 cd app
 cp ../target/hello-1.0-SNAPSHOT.jar .
+echo \# Download java binary
 curl -LO https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.5%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.5_10.tar.gz | tar xzv
+echo \# Debug: Show files
+ls -la
 cd ..
+echo \# Create slug.tgz
 tar czfv slug.tgz ./app
 
 curl -X POST \
