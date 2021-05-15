66a1ab56d948470e98b64df8e3da8b0114815414
Taz Jackson <tatjsn@users.noreply.github.com>
2021-05-15 20:17:58 +0100

    Replace Firebase Auth with Auth0

--- src/main/java/com/example/tj/HelloTjApplication.java
+++ src/main/java/com/example/tj/HelloTjApplication.java
@@ -1,20 +1,21 @@
 package com.example.tj;
 
+import com.auth0.client.auth.AuthAPI;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.example.tj.resources.HelloResource;
 import com.example.tj.resources.SigninResource;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.template.soy.jbcsrc.api.SoySauceBuilder;
 import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.jdbi3.JdbiFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 public class HelloTjApplication extends Application<HelloTjConfiguration> {
@@ -31,24 +32,27 @@ public class HelloTjApplication extends Application<HelloTjConfiguration> {
     @Override
     public void initialize(final Bootstrap<HelloTjConfiguration> bootstrap) {
         bootstrap.addBundle(new AssetsBundle("/assets", "/assets"));
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
     }
 
     @Override
     public void run(final HelloTjConfiguration configuration,
                     final Environment environment) throws IOException {
-        final var json = System.getenv("GOOG_CREDS");
-        final var jsonInputStream = new ByteArrayInputStream((json.getBytes()));
-        final var options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(jsonInputStream))
+        final var auth = new AuthAPI(configuration.getAuthDomain(), configuration.getAuthClientId(), configuration.getAuthClientSecret());
+        final var algorithm = Algorithm.HMAC256(configuration.getAuthClientSecret());
+        final var verifier = JWT.require(algorithm)
+                .withIssuer("https://" + configuration.getAuthDomain() + "/")
                 .build();
-        FirebaseApp.initializeApp(options);
-
         final var factory = new JdbiFactory();
         final var jdbi = factory.build(environment, configuration.getDataSourceFactory(), "postgresql");
         final var create = DSL.using(SQLDialect.POSTGRES);
         final var templates = new SoySauceBuilder().build();
         environment.jersey().register(new HelloResource(jdbi, create, templates));
-        environment.jersey().register(new SigninResource(templates));
+        environment.jersey().register(new SigninResource(auth, verifier, templates));
     }
 
 }
--- src/main/java/com/example/tj/resources/SigninResource.java
+++ src/main/java/com/example/tj/resources/SigninResource.java
@@ -1,26 +1,39 @@
 package com.example.tj.resources;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseToken;
+import com.auth0.client.auth.AuthAPI;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.template.soy.jbcsrc.api.SoySauce;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import java.time.Duration;
+import java.util.Map;
 
 @Path("/signin")
 @Produces(MediaType.TEXT_HTML)
 public class SigninResource {
+    private final AuthAPI auth;
+    private final JWTVerifier verifier;
     private final SoySauce templates;
 
-    public SigninResource(SoySauce templates) {
+    public SigninResource(AuthAPI auth, JWTVerifier verifier, SoySauce templates) {
+        this.auth = auth;
+        this.verifier = verifier;
         this.templates = templates;
     }
 
     @GET
-    public String index() {
+    public String index(@Context UriInfo uriInfo) {
+        final var authUrl = auth.authorizeUrl(uriInfo.getBaseUriBuilder().path("/signin/callback").build().toString())
+                .withScope("openid")
+                .withResponseType("id_token")
+                .build() + "&response_mode=form_post&nonce=deadbeef";
         return templates.renderTemplate("examples.simple.signin")
+                .setData(Map.of(
+                        "authUrl", authUrl
+                ))
                 .renderHtml()
                 .get()
                 .toString();
@@ -29,15 +42,15 @@ public class SigninResource {
     @Path("/callback")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @POST
-    public Response callback(@FormParam("token") String idToken, @Context UriInfo uriInfo) {
-        FirebaseToken decodedToken;
+    public Response callback(@FormParam("id_token") String idToken, @Context UriInfo uriInfo) {
+        final DecodedJWT jwt;
         try {
-            decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
-        } catch (FirebaseAuthException e) {
-            throw new WebApplicationException("Id token verification failed.");
+            jwt = verifier.verify(idToken);
+        } catch (JWTVerificationException exception) {
+            throw new WebApplicationException(exception);
         }
         return Response.seeOther(uriInfo.getBaseUriBuilder().path("/").build())
-                .cookie(new NewCookie("token", decodedToken.getUid(),
+                .cookie(new NewCookie("token", jwt.getSubject(),
                         "/", null, null,
                         (int) Duration.ofDays(365).toSeconds(), true, true))
                 .build();
